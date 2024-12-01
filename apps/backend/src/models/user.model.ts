02827de6b8ea7e613ad36e41d1116a6c91c33c98
coresyt <corejsyt2011@gmail.com>
2024-11-30 18:29:14 -0600

    feat(./apps/backend): Create users models

---
+++ apps/backend/src/models/user.model.ts
@@ -0,0 +1,28 @@
+import bcrypt from 'bcrypt'
+import { Schema, model } from 'mongoose'
+
+const userModel = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    statics: {
+      encryptPassword: async (password: string) => {
+        const salt = await bcrypt.genSalt()
+        return await bcrypt.hash(password, salt)
+      },
+    },
+    methods: {
+      comparePassword: async function (password: string) {
+        return await bcrypt.compare(password, this.password)
+      },
+    },
+  },
+)
+
+export default model('User', userModel)
